b5a0577baa8218897ddfc9d901161ccc321695ad
marcos2004m <109930600+marcos2004m@users.noreply.github.com>
2022-09-15 21:10:26 -0300

    Add files via upload

---
+++ academia de entrenamiento/js/questions2.js
@@ -0,0 +1,22 @@
+(function(){
+    const titleQuestions =[...document.querySelectorAll('.questions__title')];
+    console.log(titleQuestions)
+    titleQuestions.forEach(questions =>{
+    questions.addEventListener('click', ()=>{
+        let height =0;
+        let answer = questions.nextElementSibling;
+        let addpadding = questions.parentElement.parentElement;
+    
+    
+        addpadding.classList.toggle('questions__padding--add');
+    
+        questions.children[0].classList.toggle('questions__arrow--rotate');
+    
+        if(answer.clientHeight === 0){
+            height = answer.scrollHeight;
+        }
+    
+        answer.style.height = `${height}px`
+    })
+    });
+    })();
